61554a716844b8012d7beee87f1f888c7f159c47
Arnold Sebastian <arnold_sebastian_16@hotmail.com>
2018-06-20 02:52:21 +0700

    optimized project for zeit's now deployment

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const jsdom = require("jsdom");
-const JSDOM = jsdom.JSDOM;
-
-if (global) {
-  global.document = new JSDOM("index.html").window.document;
-}
-
-const subheading = document.getElementById('subheading');
-subheading.innerHTML = "Hello world";
